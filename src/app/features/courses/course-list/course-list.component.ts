e138d883649eba89330821876510bde59bbebcab
Piero Cascio <pierocascio@ecabs.com.mt>
2020-03-31 14:37:11 +0200

    Add card list component to show the list of courses.

---
+++ src/app/features/courses/course-list/course-list.component.ts
@@ -0,0 +1,21 @@
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
+import { Course }                                  from '../model/course';
+
+@Component({
+  selector: 'app-course-list',
+  templateUrl: './course-list.component.html',
+  styleUrls: ['./course-list.component.css']
+})
+export class CourseListComponent implements OnInit, AfterViewInit {
+
+  @Input() courses: Course[];
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+  ngAfterViewInit(): void {
+    console.log("Courses:", this.courses );
+  }
+}
