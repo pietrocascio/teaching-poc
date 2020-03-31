c82c6d93ed199b8db19d17bbcf1ae44f0a050ab8
Piero Cascio <pierocascio@ecabs.com.mt>
2020-03-31 14:31:18 +0200

    Add shared Material Design module.

---
+++ src/app/shared/material/material.module.ts
@@ -0,0 +1,27 @@
+import { NgModule }         from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatButtonModule }  from '@angular/material/button';
+import { MatCardModule }    from '@angular/material/card';
+import { MatDialogModule }  from '@angular/material/dialog';
+import { MatIconModule }    from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTableModule }   from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+const materialModules = [
+	MatToolbarModule,
+	MatIconModule,
+	MatSidenavModule,
+	MatCardModule,
+  MatButtonModule,
+	MatDialogModule,
+	MatTableModule
+];
+
+@NgModule( {
+	declarations: [],
+	imports: [ FlexLayoutModule, ...materialModules ],
+	exports: [ FlexLayoutModule, ...materialModules ]
+
+} )
+export class MaterialModule {}
